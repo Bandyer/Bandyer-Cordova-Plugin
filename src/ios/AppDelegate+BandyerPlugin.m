f454d28c242e5aa986f5f10ab073f728f8ff43d9
Marco Brescianini <m.brescianini85@gmail.com>
2019-06-19 14:35:21 +0200

    Refactor appdelegate category extracting swizzler class

--- src/ios/AppDelegate+BandyerPlugin.m
+++ src/ios/AppDelegate+BandyerPlugin.m
@@ -4,6 +4,7 @@
 //
 
 #import "AppDelegate+BandyerPlugin.h"
+#import "BCPSwizzler.h"
 
 @implementation AppDelegate (BandyerPlugin)
 
@@ -15,56 +16,22 @@
 + (void)swizzleReceiveIncomingPushPayload {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
         SEL originalSelector = @selector(pushRegistry:didReceiveIncomingPushWithPayload:forType:);
         SEL swizzledSelector = @selector(bandyerPushRegistry:didReceiveIncomingPushWithPayload:forType:);
         
-        Method original = class_getInstanceMethod(class, originalSelector);
-        Method swizzled = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzled),
-                        method_getTypeEncoding(swizzled));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(original),
-                                method_getTypeEncoding(original));
-        } else {
-            method_exchangeImplementations(original, swizzled);
-        }
+        BCPSwizzler *swizzler = [[BCPSwizzler alloc] initWithClass:[self class] sourceSelector:originalSelector targetSelector:swizzledSelector];
+        [swizzler swizzle];
     });
 }
 
 + (void)swizzleReceiveIncomingPushPayloadWithCompletionHandler {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
         SEL originalSelector = @selector(pushRegistry:didReceiveIncomingPushWithPayload:forType:withCompletionHandler:);
         SEL swizzledSelector = @selector(bandyerPushRegistry:didReceiveIncomingPushWithPayload:forType:withCompletionHandler:);
         
-        Method original = class_getInstanceMethod(class, originalSelector);
-        Method swizzled = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzled),
-                        method_getTypeEncoding(swizzled));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(original),
-                                method_getTypeEncoding(original));
-        } else {
-            method_exchangeImplementations(original, swizzled);
-        }
+        BCPSwizzler *swizzler = [[BCPSwizzler alloc] initWithClass:[self class] sourceSelector:originalSelector targetSelector:swizzledSelector];
+        [swizzler swizzle];
     });
 }
 
