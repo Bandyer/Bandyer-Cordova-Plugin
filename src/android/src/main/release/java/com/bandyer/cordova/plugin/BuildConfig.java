9f117b4c6a30319f1f6e0f2474951a90bb9c918d
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2019-10-11 15:46:42 +0200

    support android 10

--- src/android/src/main/release/java/com/bandyer/cordova/plugin/BuildConfig.java
+++ src/android/src/main/release/java/com/bandyer/cordova/plugin/BuildConfig.java
@@ -5,6 +5,11 @@ package com.bandyer.cordova.plugin;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.bandyer.cordova.plugin";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.bandyer.cordova.plugin";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
