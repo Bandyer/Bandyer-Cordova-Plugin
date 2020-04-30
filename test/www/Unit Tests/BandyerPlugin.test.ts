cde44d7f2ad06412ad757c84dcc0d3fdd5d502d1
Marco Brescianini <marco.brescianini@bandyer.com>
2020-04-30 18:23:15 +0200

    Add test case verifying plugin setup happy path

--- test/www/Unit Tests/BandyerPlugin.test.ts
+++ test/www/Unit Tests/BandyerPlugin.test.ts
@@ -61,11 +61,60 @@ describe('Plugin setup', function () {
 
         expect(setup).toThrowError(Error);
     });
+
+    test('Calls "initializeBandyer" action with the argument provided in config parameter', () => {
+        var config = {
+            environment: Environments.sandbox(),
+            appId: 'mAppId_a21393y2a4ada1231',
+            logEnabled: true,
+            androidConfig : {
+                callEnabled: true,
+                fileSharingEnabled: true,
+                screenSharingEnabled: true,
+                chatEnabled: true,
+                whiteboardEnabled: true,
+            },
+            iosConfig : {
+                voipNotificationKeyPath: 'VoIPKeyPath',
+                fakeCapturerFileName: 'filename.mp4',
+                callkit: {
+                    enabled: true,
+                    appIconName: "AppIcon",
+                    ringtoneSoundName: "ringtone.mp3"
+                }
+            }
+        }
+
+        BandyerPlugin.setup(config);
+
+        expect(cordovaSpy.execInvocations.length).toEqual(1);
+        expect(cordovaSpy.execInvocations[0].service).toMatch('BandyerPlugin');
+        expect(cordovaSpy.execInvocations[0].action).toMatch('initializeBandyer');
+        expect(cordovaSpy.execInvocations[0].args).toBeDefined();
+        expect(cordovaSpy.execInvocations[0].args[0]).toBeDefined();
+        expect(cordovaSpy.execInvocations[0].args[0]["environment"]).toMatch('sandbox');
+        expect(cordovaSpy.execInvocations[0].args[0]["appId"]).toMatch('mAppId_a21393y2a4ada1231');
+        expect(cordovaSpy.execInvocations[0].args[0]["logEnabled"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["ios_callkit"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["ios_fakeCapturerFileName"]).toMatch('filename.mp4');
+        expect(cordovaSpy.execInvocations[0].args[0]["ios_voipNotificationKeyPath"]).toMatch('VoIPKeyPath');
+        expect(cordovaSpy.execInvocations[0].args[0]["android_isCallEnabled"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["android_isFileSharingEnabled"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["android_isScreenSharingEnabled"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["android_isChatEnabled"]).toBeTruthy();
+        expect(cordovaSpy.execInvocations[0].args[0]["android_isWhiteboardEnabled"]).toBeTruthy();
+    })
 });
 
 function makePluginConfig(): BandyerPluginConfigs {
     return {
-        androidConfig: {},
+        androidConfig: {
+            callEnabled: true,
+            fileSharingEnabled: true,
+            screenSharingEnabled: true,
+            chatEnabled: true,
+            whiteboardEnabled: true,
+        },
         appId: 'Some APP ID',
         environment: Environments.sandbox(),
         iosConfig: {
